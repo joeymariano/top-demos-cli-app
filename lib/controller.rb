a5994b9a9f83eb5b28a71975925e8a24479c1e91
joey michalina mariano <joey.michalina.mariano@gmail.com>
2017-10-18 20:42:18 +0000

    added call list_demos and menu to controller class

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,7 +1,21 @@
 class Controller
 
-  def self.call
-    puts "Welcome to Top Demos"
+  def call
+    list_demos
+    menu
+  end
+
+  def list_demos
+    puts "Here come the top demos from Pouet.net.  Awesome."
+    puts <<-DOC
+      1. Waillee by Prismbeings
+         https://www.youtube.com/watch?v=CDDedA-x2-k
+      2. hold on by holon
+    DOC
+  end
+
+  def menu
+    puts "Which demo # would you like more information on?"
   end
 
 end
