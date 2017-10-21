63dcc840f96d9b69b6e169675e39d2f1ef2b80e4
joey michalina mariano <joey.michalina.mariano@gmail.com>
2017-10-21 23:53:59 +0000

    scraper now makes 10 demos

--- lib/demo.rb
+++ lib/demo.rb
@@ -1,10 +1,19 @@
 class Demo
-  attr_accessor :name, :author, :url
+  attr_accessor :title, :group, :url, :type, :platform
+  @@all = []
 
-  def initialize(name, author, url)
-    @name = name
-    @author = author
+  def initialize(title, group, url, type, platform)
+    @title = title
+    @group = group
     @url = url
+    @type = type
+    @platform = platform
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
 
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,14 +1,24 @@
 class Scraper
-  attr_accessor :homepage, :artist_page
 
   def initialize
-    @homepage = Nokogiri::HTML(open("http://pouet.net/"))
-    binding.pry
+    doc = Nokogiri::HTML(open("http://pouet.net/"))
+    create_demos(doc)
   end
 
-  def create_demos
-    @homepage
+  def create_demos(doc)
+    title = nil
+    group = nil
+    url = nil
+    type = nil
+    platform = nil
+    doc.css('#pouetbox_topmonth ul.boxlist span').each do |demo|
+      title = demo.css('span.prod').text
+      group = demo.css('span.group a').text
+      url = demo.at_css('span.prod a')['href']
+      type = demo.css('span.typeiconlist').text.chomp
+      platform = demo.css('span.platformiconlist').text.chomp
+      Demo.new(title, group, url, type, platform)
+    end
   end
-
 
 end
