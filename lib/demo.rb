d71d4b3652c2f076189258ad574d21ac1060a02f
joey michalina mariano <joey.michalina.mariano@gmail.com>
2017-10-22 01:35:40 +0000

    added more attributes to demos with get_frm_demos_pg

--- lib/demo.rb
+++ lib/demo.rb
@@ -1,19 +1,20 @@
 class Demo
-  attr_accessor :title, :group, :url, :type, :platform
+  attr_reader :title, :group, :url, :type, :platform, :youtube, :file_url
   @@all = []
 
-  def initialize(title, group, url, type, platform)
+  def initialize(title, group, url, type, platform, youtube, file_url)
     @title = title
     @group = group
     @url = url
     @type = type
     @platform = platform
+    @youtube = youtube
+    @file_url = file_url
     @@all << self
   end
 
   def self.all
     @@all
   end
-
 
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,14 +11,27 @@ class Scraper
     url = nil
     type = nil
     platform = nil
-    doc.css('#pouetbox_topmonth ul.boxlist li').each do |demo|
+    doc.css('#pouetbox_topmonth li').each do |demo|
       title = demo.css('span.prod').text
       group = demo.css('span.group a').text
-      url = demo.at_css('span.prod a')['href']
+      url = "http://pouet.net/" + demo.at_css('span.prod a')['href']
       type = demo.css('span.typeiconlist').text.chomp
       platform = demo.css('span.platformiconlist').text.chomp
-      Demo.new(title, group, url, type, platform)
+      additional_info = get_frm_demos_pg(url)
+      youtube = additional_info[1]
+      file_url = additional_info[0]
+      Demo.new(title, group, url, type, platform, youtube, file_url)
     end
+    binding.pry
+  end
+
+  def get_frm_demos_pg(url)
+    result = []
+    demo_doc = Nokogiri::HTML(open(url))
+    demo_doc.css('#links ul li').each do |link|
+      result << link.at('a').attr('href')
+    end
+    result
   end
 
 end
